f45dd850c51c92acaf3655a56ec7dd407756e7a1
jozwang <9334262+jozwang@users.noreply.github.com>
2025-03-20 18:49:41 +1100

    Update realtime.py

--- realtime.py
+++ realtime.py
@@ -154,10 +154,13 @@ if not display_df.empty:
             folium.Marker(
                 location=[row["lat"], row["lon"]],
                 popup=f"Vehicle ID: {row['vehicle_id']}<br>Stop Sequence: {row['Stop Sequence']}",
-                label=folium.DivIcon(html=f'<div style="font-size: 12px; font-weight: bold; color: black; background-color: white; padding: 4px; border-radius: 3px;">{row["vehicle_id"]} - {row["Stop Sequence"]}</div>'),
                 icon=folium.Icon(color=color)
             ).add_to(m)
 
+            folium.Marker(
+                location=[row["lat"], row["lon"]],
+                icon=folium.DivIcon(html=f'<div style="font-size: 12px; font-weight: bold; color: black; background-color: white; padding: 4px; border-radius: 3px;">{row["vehicle_id"]} - {row["Stop Sequence"]}</div>')
+            ).add_to(m)
         
         folium_static(m)
     
