8a8286ff681ede43bb024ecaa6bfaebbff92e0c0
jozwang <9334262+jozwang@users.noreply.github.com>
2025-07-26 13:26:28 +1000

    Update realtime.py

--- realtime.py
+++ realtime.py
@@ -8,7 +8,7 @@ from google.transit import gtfs_realtime_pb2
 from datetime import datetime, timedelta
 import time
 import pytz
-import get_vehicle_updates from gtfs_realtime
+from gtfs_realtime import get_vehicle_updates 
 
 
 # Streamlit App
